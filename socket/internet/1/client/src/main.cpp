570895bcdf6465c5eecea45d40a05dbc41deffe7
vtsyganenko <tsyganenko90@yandex.ru>
2019-08-21 15:09:52 +0300

    improve inet socket example

--- socket/internet/1/client/src/main.cpp
+++ socket/internet/1/client/src/main.cpp
@@ -20,7 +20,6 @@ void error(const char* msg)
 int main()
 {
     char buf[25];
-    const char* SOCK_PATH = "../../socket";
 
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(socket_fd == -1) error("socket not created");
@@ -32,26 +31,32 @@ int main()
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(60000);
-/*
-    int connect_res = connect(socket_fd, (struct sockaddr*)&addr, sizeof(addr.sun_path));
-    if(connect_res == -1) error("connect error");
-    else std::cout << "connect result " << connect_res << std::endl;
-    std::cout << "Client ready to send:" << std::endl;
-*/
+
     ssize_t readingSize = 0;
     // reading from stdin
     while( (readingSize = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
     {
         // and send it to server
-        //if (write(socket_fd, buf, readingSize) != readingSize)
         if(sendto(socket_fd, buf, readingSize, 0,
-               (struct sockaddr*)&addr, sizeof(addr)) != readingSize)
+                 (struct sockaddr*)&addr, sizeof(addr)) != readingSize)
         {
             error("write error");
             exit(EXIT_FAILURE);
         }
 
         if(readingSize < 0) error("reading error");
+
+        // wait for server responce
+        memset(&buf, 0, sizeof(buf));
+        int receive_bytes = recvfrom(socket_fd, buf, sizeof(buf), 0,
+                                     nullptr, nullptr);
+        if(receive_bytes < 0) error("receive error");
+        else {
+            std::cout << "responce from server " << receive_bytes << " bytes" << std::endl;
+            std::cout << "data=[" << buf << "] " << sizeof(buf) << std::endl;
+        }
+
+        memset(&buf, 0, sizeof(buf));
     }
 
     close(socket_fd);
--- socket/internet/1/server/src/main.cpp
+++ socket/internet/1/server/src/main.cpp
@@ -20,7 +20,6 @@ void error(const char* msg)
 int main()
 {
     char buf[25];
-    //const char* SOCK_PATH = "../../socket";
 
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(socket_fd == -1) error("socket not created");
@@ -33,45 +32,27 @@ int main()
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     addr.sin_port = htons(60000);
 
-
-    // delete name from filesystem before bind
-    //unlink(SOCK_PATH);
-
-
-
     int bind_res = bind(socket_fd, (struct sockaddr*)&addr, sizeof(addr));
     if(bind_res == -1) error("bind fail");
     else std::cout << "bind result " << bind_res << std::endl;
 
-//    int listen_res = listen(socket_fd, 0);
-//    if(listen_res == -1) error("listen fail");
-//    else std::cout << "listen result " << listen_res << std::endl;
-
-    int client_socket_fd = 0;
     ssize_t readingSize = 0;
     while(true)
     {
-//        struct sockaddr_un clientInfo;
-//        memset(&clientInfo, 0, sizeof(clientInfo));
-//        socklen_t clientInfoSize = sizeof(clientInfo);
-//        if((client_socket_fd = accept(socket_fd, (struct sockaddr*)&clientInfo, &clientInfoSize)) == -1)
-
-        //client_socket_fd = accept(socket_fd, nullptr, nullptr);
-        //if(client_socket_fd == -1) error("accept error");
-        //else std::cout << "client socket fd = " << client_socket_fd << std::endl;
-
         std::cout << "Server ready to read" << std::endl;
 
         struct sockaddr_in client_addr;
         memset(&client_addr, 0, sizeof(client_addr));
-
         socklen_t client_len = sizeof(client_addr);
 
         while( (readingSize = recvfrom(socket_fd, buf, sizeof(buf), 0, (struct sockaddr*)&client_addr, &client_len)) > 0 )
         {
+            std::cout << "new incoming message:" << std::endl;
+            std::cout << "port " << ntohs(client_addr.sin_port) << " address " << inet_ntoa(client_addr.sin_addr) << std::endl;
+            std::cout << "len " << client_len << " reading size " << readingSize << std::endl;
             std::cout << "read " << readingSize << " bytes: [" << buf << "]" << std::endl;
 
-/*
+            // if message is "q" then exit
             std::string s(buf);
             if(!s.empty())
             {
@@ -79,15 +60,25 @@ int main()
                 std::cout << "processing string: [" << s << "] size = " << s.size() << std::endl;
 
                 // "protocol"
-                if(s == std::string("exit"))
+                if(s == std::string("q"))
                 {
                     std::cout << "exit command from client" << std::endl;
-                    close(client_socket_fd);
                     close(socket_fd);
                     exit(EXIT_SUCCESS);
                 }
             }
-            */
+
+            // process data
+            for(int i=0; i<readingSize; ++i)
+                buf[i] = toupper((unsigned char) buf[i]);
+            std::cout << "buf size " << sizeof(buf) << std::endl;
+
+            // sending back
+            int send = sendto(socket_fd, buf, readingSize, 0,
+                             (struct sockaddr*)&client_addr, client_len);
+            if(send != readingSize) error("send error");
+            else std::cout << "send " << send << " bytes" << std::endl;
+
             // clear buf
             memset(&buf, 0, sizeof(buf));
         }
@@ -97,12 +88,9 @@ int main()
         if(readingSize == 0)
         {
             printf("EOF (client disconnected)\n");
-            close(client_socket_fd);
             break;
         }
     }
-
-
 
     close(socket_fd);
     std::cout << "Server stopped" << std::endl;
