3a9a4b1c479d0aa4874be64f4f3c589b41b221a2
vtsyganenko <tsyganenko90@yandex.ru>
2020-01-09 22:51:17 +0300

    completed merge sort in pure C

--- algorithms/04 - quicksort/cpp/mergesort/main.c
+++ algorithms/04 - quicksort/cpp/mergesort/main.c
@@ -19,7 +19,7 @@ void print_array(const int* array, unsigned int size)
     }
 }
 
-// swap elements in array which size is 2
+// swap elements in array, which size is 2
 void swap_array_2(int* array, unsigned int size)
 {
     if(size == 2)
@@ -33,7 +33,7 @@ void swap_array_2(int* array, unsigned int size)
 // divide array to left and right parts
 void divide_array(const int* array, const unsigned int size, int** left, unsigned int* left_size, int** right, unsigned int* right_size)
 {
-    printf("divide_array\n");
+    //printf("divide_array\n");
     if(size == 0)
     {
         (*left) = NULL;
@@ -47,14 +47,14 @@ void divide_array(const int* array, const unsigned int size, int** left, unsigne
     unsigned int right_part_size = size - left_part_size;
 
     *left = malloc(left_part_size * sizeof(int));
-    printf("call malloc addr %p size %d \n", (void*)(*left), left_part_size);
+    //printf("call malloc addr %p size %d \n", (void*)(*left), left_part_size);
     for(unsigned int i=0; i< left_part_size; ++i)
     {
         (*left)[i] = array[i];
     }
 
     *right = malloc(right_part_size * sizeof(int));
-    printf("call malloc addr %p size %d \n", (void*)(*right), right_part_size);
+    //printf("call malloc addr %p size %d \n", (void*)(*right), right_part_size);
     int right_index = 0;
     for(unsigned int i=left_part_size; i<size; ++i)
     {
@@ -68,7 +68,11 @@ void divide_array(const int* array, const unsigned int size, int** left, unsigne
 // merge two sorted arrays into one
 void merge_sorted_arrays(const int* array1, const unsigned int len1, const int* array2, const unsigned int len2, int* result_array, unsigned int* result_len)
 {
-    printf("merge_sorted_arrays\n");
+    //printf("merge_sorted_arrays\n");
+    //printf("left: ");
+    //print_array(array1, len1);
+    //printf("right: ");
+    //print_array(array2, len2);
     unsigned int full_length = len1 + len2;
     if(full_length == 0)
     {
@@ -76,6 +80,10 @@ void merge_sorted_arrays(const int* array1, const unsigned int len1, const int* 
         *result_len = 0;
         return;
     }
+
+    // for clear test
+    for(unsigned int i=0; i<full_length; ++i)
+        result_array[i] = 0;
 
     unsigned int index1 = 0;
     unsigned int index2 = 0;
@@ -116,9 +124,8 @@ void merge_sorted_arrays(const int* array1, const unsigned int len1, const int* 
                 index1++;
             }
         }
-
         if(index2 != len2) {
-            unsigned int residue = len1 - index2;
+            unsigned int residue = len2 - index2;
             for(unsigned int i=0; i<residue; ++i)
             {
                 result_array[indexR] = array2[index2];
@@ -133,9 +140,9 @@ void merge_sorted_arrays(const int* array1, const unsigned int len1, const int* 
 
 int* merge_sort(int* array, unsigned int size)
 {
-    printf("-----------------\n");
-    printf("merge_sort:  ");
-    print_array(array, size);
+    //printf("-----------------\n");
+    //printf("merge_sort:  ");
+    //print_array(array, size);
 
     if(size == 2)
     {
@@ -156,14 +163,13 @@ int* merge_sort(int* array, unsigned int size)
         left = merge_sort(left, left_size);
         right = merge_sort(right, right_size);
 
-        //printf("\n before merge: \n");
-        //print_array(array, size);
+        // merge sorted lists
         merge_sorted_arrays(left, left_size, right, right_size, array, &size);
 
         free(left);
-        printf("call free addr %p \n", (void*)(left));
+        //printf("call free addr %p \n", (void*)(left));
         free(right);
-        printf("call free addr %p \n", (void*)(right));
+        //printf("call free addr %p \n", (void*)(right));
 
         return array;
     }
@@ -173,14 +179,33 @@ int* merge_sort(int* array, unsigned int size)
     }
 }
 
+void test_merge_sort(int* array, unsigned int size)
+{
+    printf("before ");
+    print_array(array, size);
+    merge_sort(array, size);
+    printf("after ");
+    print_array(array, size);
+    printf("-----------------\n");
+}
+
 int main()
 {
-    const int N1 = 8;
-    int arr1[] = {10, 44, 6, 9, 1, 22, 8, 0};
 
-    print_array(arr1, N1);
-    merge_sort(arr1, N1);
-    print_array(arr1, N1);
+    int arr0[] = {};
+    test_merge_sort(arr0, 0);
+
+    int arr1[] = {18};
+    test_merge_sort(arr1, 1);
+
+    int arr2[] = {10, 44, 6, 9, 1, 22, 8, 0};
+    test_merge_sort(arr2, 8);
+
+    int arr3[] = {23, 14, 0, 14, -7, -58, 0};
+    test_merge_sort(arr3, 7);
+
+    int arr4[] = {-3, 4, 1, 23, 12, 0, 9, 8, 23, 12, 0, 14, 7, 58, 30};
+    test_merge_sort(arr4, 15);
 
     printf("end!\n");
     return 0;
