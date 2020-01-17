e12f162d10821d96b9c12a2b936161ac4fcfe08b
vtsyganenko <tsyganenko90@yandex.ru>
2020-01-17 15:17:58 +0300

    started hash table impl

---
+++ algorithms/05 - hash table/chain table cpp/ChainHashTable/chainhashtable.h
@@ -0,0 +1,93 @@
+#ifndef CHAINHASHTABLE_H
+#define CHAINHASHTABLE_H
+
+#include "hash.h"
+
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+
+
+
+template <class Key, class Type, class HashFunc = Hash<Key>>
+class ChainHashTable
+{
+public:
+    ChainHashTable();
+    ~ChainHashTable();
+
+    void add(const Key& key, const Type& value);
+
+    void print();
+
+private:
+    template <class T>
+    struct Node
+    {
+        std::list<T> chain;
+    };
+    std::vector< Node<Type>* > data_;
+
+    std::size_t n_;
+    HashFunc hash_;
+
+};
+
+template <class Key, class Type, class HashFunc>
+ChainHashTable<Key, Type, HashFunc>::ChainHashTable()
+    : n_(256), hash_(HashFunc(n_))
+{
+    std::cout << "ChainHashTable ctor" << std::endl;
+    //n_ = 256;
+    data_.resize(n_, nullptr);
+}
+
+template <class Key, class Type, class HashFunc>
+ChainHashTable<Key, Type, HashFunc>::~ChainHashTable()
+{
+    for(auto i=0; i<data_.size(); ++i)
+    {
+        if(data_[i]) delete data_[i];
+    }
+}
+
+template <class Key, class Type, class HashFunc>
+void ChainHashTable<Key, Type, HashFunc>::add(const Key& key, const Type& value)
+{
+    std::cout << "add: key = " << key << " value = " << value << std::endl;
+
+    std::size_t hash = hash_(key);
+    std::cout << "add: hash = " << hash << std::endl;
+
+    if(data_[hash])
+    {
+        data_[hash]->chain.push_back(value);
+    }
+    else
+    {
+        data_[hash] = new Node<Type>;
+        data_[hash]->chain.push_back(value);
+    }
+
+}
+
+template <class Key, class Type, class HashFunc>
+void ChainHashTable<Key, Type, HashFunc>::print()
+{
+    for(int i=0; i<data_.size(); ++i)
+    {
+        if(data_[i])
+        {
+            std::cout << "hash [" << i << "] value [";
+            for(const auto& elem : data_[i]->chain)
+            {
+                std::cout << elem << "; ";
+            }
+            std::cout << "]" << std::endl;
+        }
+    }
+}
+
+#endif // CHAINHASHTABLE_H
---
+++ algorithms/05 - hash table/chain table cpp/ChainHashTable/main.cpp
@@ -0,0 +1,48 @@
+#include "chainhashtable.h"
+
+#include <iostream>
+#include <unordered_map>
+
+struct Team
+{
+    int points;
+    std::string name;
+
+    bool operator==(const Team& other) const
+    {
+        return name == other.name;
+    }
+};
+
+struct MyHash
+{
+    std::size_t operator() (const Team& team) const noexcept
+    {
+        // ...
+        return 0;
+    }
+};
+
+int main()
+{
+    //Team d1 = {30, "Dynamo"};
+    //Team d2 = {32, "Spartak"};
+    //Team d3 = {35, "CSKA"};
+
+    ChainHashTable<int, std::string> table;
+
+    table.add(10, "spartak");
+    table.add(20, "dynamo");
+    table.add(30, "cska");
+    table.add(31, "cska");
+    table.add(32, "cska");
+    table.add(33, "cska");
+    table.add(34, "cska");
+    table.add(35, "cska");
+    table.add(36, "cska");
+
+    table.print();
+
+
+    return 0;
+}
