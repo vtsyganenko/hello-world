05a7e54fbd2d0f34e7adb1b1fea6a0c01c964e24
vtsyganenko <tsyganenko90@yandex.ru>
2020-09-12 19:42:16 +0300

    added packaged_task

--- cpp/002/promise_future/main.cpp
+++ cpp/002/promise_future/main.cpp
@@ -71,10 +71,38 @@ void example2()
     printMessage("Main thread finishes example 2");
 }
 
+void example3()
+{
+    printMessage("Main thread start example 3");
+
+    auto function = [](double value1, double value2) -> double
+    {
+        printMessage("threadFunction starts work with value: " + std::to_string(value1) + " and value: " + std::to_string(value2));
+        std::this_thread::sleep_for(std::chrono::seconds(3));   // calculations ...
+        printMessage("threadFunction ends work");
+        return value1*value2;
+    };
+
+    std::packaged_task<double(double, double)> task(function);
+    std::future<double> ft = task.get_future();
+
+    double value1 = 25;
+    double value2 = 10;
+    std::thread t(std::move(task), value1, value2);
+
+    printMessage("Main thread wait for result");
+    double result = ft.get();
+    printMessage("result = " + std::to_string(result));
+
+    t.join();   // ! async hides this
+    printMessage("Main thread finishes example 3");
+}
+
 int main()
 {
     example1(); // async + future
     example2(); // thread + promise + future
+    example3(); // thread + packaged_task + future
 
     return 0;
 }
