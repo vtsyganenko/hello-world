3a02b1a0c85d3ee436290ccb3de163bca9be44f9
vtsyganenko <tsyganenko90@yandex.ru>
2023-01-16 11:34:26 +0300

    obj-c: updated example with additional string methods

--- obj-c/19_other_strings_methods/main.m
+++ obj-c/19_other_strings_methods/main.m
@@ -4,6 +4,11 @@
 int main(int argc, const char* argv[]) {
     @autoreleasepool {
 
+        void (^separator) (void) = 
+            ^(void) {
+                NSLog(@"\n");
+            };
+
         // data from file
         NSString* filePath = @"../file.txt";
         //                                                           NSStringEncoding                NSError
@@ -19,8 +24,9 @@ int main(int argc, const char* argv[]) {
 
         url = [NSURL URLWithString: @"http://google.com"];
         str = [NSString stringWithContentsOfURL: url encoding: NSASCIIStringEncoding error: NULL];
-        //NSLog(@"string from URL \"%@\":", url);
-        //NSLog(@"%@", str);
+        NSLog(@"string from URL \"%@\":", url);
+        NSLog(@"%@", str);
+        separator();
 
         // create an empty string
         NSString* es = [NSString string];
@@ -32,6 +38,7 @@ int main(int argc, const char* argv[]) {
         // string with format
         str = [NSString stringWithFormat: @"number %@ string %@ string %s float %f", @150, @"Hello", [ems UTF8String], 22.4];
         NSLog(@"%@", str);
+        separator();
 
         // functions above have init- versions: initWithString, initWithContentsOfFile, initWithContentsOfURL
 
@@ -40,6 +47,7 @@ int main(int argc, const char* argv[]) {
         for(NSUInteger i=0; i < [str length]; ++i) {
             NSLog(@"character at index %lu is %c", i, [str characterAtIndex: i]);
         }
+        separator();
 
         // comparison (lexicographic order, case insensitive)
         NSString* firstString = @"abc";
@@ -54,6 +62,7 @@ int main(int argc, const char* argv[]) {
         secondString = @"abcd";
         res = [firstString caseInsensitiveCompare: secondString];
         NSLog(@"case insensitive comparison: %@ and %@ is %ld", firstString, secondString, res);     // NSOrderedAscending (first < second)
+        separator();
 
         // prefix and suffix
         firstString = @"abc";
@@ -64,6 +73,36 @@ int main(int argc, const char* argv[]) {
         NSLog(@"%@ is prefix for %@ : %d", secondString, str, [str hasPrefix: secondString]);
         NSLog(@"%@ is suffix for %@ : %d", secondString, str, [str hasSuffix: secondString]);
         NSLog(@"%@ is suffix for %@ : %d", firstString, str, [str hasSuffix: firstString]);
+
+        // capitalized string
+        str = @"one two three four five";
+        NSLog(@"source string: %@", str);
+        str = [str capitalizedString];
+        NSLog(@"capitalized string: %@", str);
+        separator();
+
+        // getting values
+        str = @"256.1234567";
+        NSLog(@"string \"%@\" to float value is %f", str, [str floatValue]);
+        NSLog(@"string \"%@\" to double value is %f", str, [str doubleValue]);
+        str = @"3000000000";
+        NSLog(@"string \"%@\" to NSInteger %ld and to int %d", str, [str integerValue], [str intValue]);
+        str = @"2147483646";
+        NSLog(@"string \"%@\" to int %d", str, [str intValue]);
+        separator();
+
+        // next methods appear in NSMutableString
+
+        // create with capacity (creates empty string, capacity is for efficiency)
+        NSMutableString* mstr = [NSMutableString stringWithCapacity: 10];
+        NSLog(@"string with capacity 10 is \"%@\"", mstr);
+        mstr = [[NSMutableString alloc] initWithCapacity: 5];
+        NSLog(@"string with capacity 5 is \"%@\"", mstr);
+
+        // create
+        [mstr setString: @"Hello from mutable string!"];
+        NSLog(@"%@", mstr);
+
 
 
 
