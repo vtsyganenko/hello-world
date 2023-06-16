b03ff684d95d55ea0e0ad3289cc50b8e0e31b8b9
vtsyganenko <tsyganenko90@yandex.ru>
2023-06-16 22:14:51 +0300

    swift: updated functions example - divided anonymous and closures

--- swift/02_functions/anonymous.swift
+++ swift/02_functions/anonymous.swift
@@ -189,36 +189,3 @@ workWithWrapper(object:{
     return obj
 }()
 )
-
-//---------------------------------------------------------------------------------------
-
-// 4. closures
-print()
-
-class Dog {
-    var whatThisDogSays = "woof"
-
-    func bark() {
-        print(self.whatThisDogSays)
-    }
-}
-
-func doThisFunction(_ f:() -> ()) {
-    f()
-}
-
-// example 1
-let d = Dog()
-d.whatThisDogSays = "arf"
-let barkFunction = d.bark
-doThisFunction(barkFunction)    // arf
-// or
-doThisFunction(d.bark)          // arf
-
-// example 2
-let d1 = Dog()
-let barkFunc = d1.bark
-doThisFunction(barkFunc)    // woof
-d1.whatThisDogSays = "arf"
-doThisFunction(barkFunc)    // arf
-
